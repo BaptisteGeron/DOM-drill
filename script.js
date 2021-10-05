6519e173856ad31f9d6cc0ced1099920e270bef2
BaptisteGeron <baptiste.geron@gmail.com>
2021-10-05 15:54:41 +0200

    before bugfixes

---
+++ script.js
@@ -0,0 +1,128 @@
+//In the index.html add a script tag to the end of the <body>. Then do the following exercises.
+
+//DO NOT MODIFY THE HTML FILE, all the modifications should be done using javascript. Exercises that start with a (*) are harder to solve, you can keep this for the end.
+
+//Use childNodes to list all the children from the <ul>
+let listChildren = document.getElementsByTagName("ul")[0].children;
+
+//Write a for loop to iterate over every child. In this loop:Use a condition in the loop to only iterate over element nodes (see: nodeTypes. Use child.nodeType === 1)
+for (i = 0; i < listChildren.length; i++) {
+  if (listChildren[i].nodeType === Node.ELEMENT_NODE) {
+    console.log(listChildren[i]);
+  }
+}
+
+//Find the element that contains Fast and Furious. If it's not the first element move it to the top of the list. Since Fast and Furious is the most important franchise ever, add a class .important to the element
+
+function fffirst() {
+  for (i = 0; i < listChildren.length; i++) {
+    if (listChildren[i].innerHTML == "Fast and Furious") {
+      let firstEl = listChildren[0];
+      firstEl.className = "";
+      let newFirstEl = document.createElement("li");
+      newFirstEl.innerHTML = listChildren[i].innerHTML;
+      document.getElementsByTagName("ul")[0].removeChild(listChildren[i]);
+      document.getElementsByTagName("ul")[0].insertBefore(newFirstEl, firstEl);
+      newFirstEl.className = "important";
+    }
+  }
+}
+fffirst();
+
+//Add an eventListener on every items of the list. If the item is clicked an alert() pops up to display the name of clicked element. Add a special condition to the previous function if the clicked item is Fast and Furious the alert() should display The most important franchise ever, the story of DOM(inic) Toretto's family. It's not about car, it's about family.
+
+for (i = 0; i < listChildren.length; i++) {
+  listChildren[i].addEventListener("click", alertContent);
+}
+
+function alertContent(e) {
+  if (e.target.innerHTML == "Fast and Furious") {
+    alert(
+      "The most important franchise ever, the story of DOM(inic) Toretto's family. It's not about car, it's about family"
+    );
+  } 
+  else {
+    alert(e.target.innerHTML);
+  }
+}
+
+//(*) Remove duplicates using removeChild (duplicates are items with the exact same textContent, isEqualNode might be useful).
+
+for (i = 0; i < listChildren.length; i++) {
+  let iContent = listChildren[i].innerHTML;
+  for (j = 0; j < listChildren.length; j++) {
+    if (j == i) {
+      //no comparison with himself
+    } 
+    else {
+      if (iContent == listChildren[j].innerHTML) {
+        document.getElementsByTagName("ul")[0].removeChild(listChildren[j]);
+      }
+    }
+  }
+}
+
+//(*) Add an eventListener on the document body, listening for keyup. When pressing the r key of the keyboard the list should get sorted in a random order, however Fast and Furious should remain the first element (most important franchise ever, remember?)
+document.body.addEventListener("keyup", randomSort);
+function randomSort(e) {
+  if (e.keyCode == 82) {
+    for (i = listChildren.length - 1; i > 0; i--) {
+      rand = Math.floor((i + 1) * Math.random()); //get random between zero and i (inclusive)
+      temp = listChildren[rand].innerHTML; //swap i and the zero-indexed number
+      listChildren[rand].innerHTML = listChildren[i].innerHTML;
+      listChildren[i].innerHTML = temp;
+    }
+    if (
+      document.getElementsByTagName("ul")[0].firstElementChild.innerHTML !=
+      "Fast and Furious"
+    ) {
+      fffirst();
+    }
+  }
+  //(*) Modify the previous function so that when you press the letter d of your keyboard, the Fast and Furious element gets cloned
+  if (e.keyCode == 68) {
+      let cloneFF = document.getElementsByTagName("ul")[0].firstElementChild.cloneNode(true);
+      console.log("cloned " + cloneFF.innerHTML);
+  }
+}
+
+//Create a new <div> before the list, using createElement and insertBefore
+
+let newDiv = document.createElement('div');
+document.body.insertBefore(newDiv,document.getElementsByTagName("ul")[0]);
+
+//Using createElement, create a <select> tag into the previously created <div>, with two <option>s in it: "important franchises" and "normal franchises"
+
+let chooseOption = document.createElement('select');
+let option1 = document.createElement('option');
+option1.value = "important";
+option1.innerHTML = "important franchises";
+let option2 = document.createElement('option');
+option2.value = "normal";
+option2.innerHTML = "normal franchises"
+let pHolder = document.createElement('option');
+pHolder.innerHTML= "choose...";
+chooseOption.appendChild(pHolder);
+chooseOption.appendChild(option1);
+chooseOption.appendChild(option2);
+newDiv.appendChild(chooseOption);
+
+
+//Add an eventListener to the <select>, on change, if the option "important franchise" is chosen, only display items of the list that have the class .important. (hint: you can toggle visibility using element.style.visibility = 'hidden')
+
+let selectField =document.getElementsByTagName('select')[0];
+selectField.addEventListener('change',matchingItems)
+function matchingItems(e) {
+    if (e.target.value =="important"){
+        for (i = 0; i < listChildren.length; i++) {
+            if (listChildren[i].className!="important"){
+                listChildren[i].style.visibility = 'hidden';
+            }
+        }
+    }
+    else if (e.target.value=="normal"){
+        for (i = 0; i < listChildren.length; i++) {
+                listChildren[i].style.visibility = 'visible';
+            }
+        }
+    }
